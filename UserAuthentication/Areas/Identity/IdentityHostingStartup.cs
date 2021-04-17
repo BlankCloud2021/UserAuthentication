60a46f950cb580b626353148ef9bd6caeceae3fd
Kent Moseley <moseleykent856@gmail.com>
2021-04-16 21:59:45 -0400

    Add project files.

---
+++ UserAuthentication/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UserAuthentication.Areas.Identity.Data;
+using UserAuthentication.Data;
+
+[assembly: HostingStartup(typeof(UserAuthentication.Areas.Identity.IdentityHostingStartup))]
+namespace UserAuthentication.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<UserAuthenticationDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("UserAuthenticationDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<UserAuthenticationDbContext>();
+            });
+        }
+    }
+}
